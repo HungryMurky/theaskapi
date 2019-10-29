9ccc04288ca888bd90897813681bccee2494081c
Sergey Astakhov <s.astakhau@gmail.com>
2019-10-29 12:29:12 +0000

    CI setup

--- src/main/java/com/theask/theaskapi/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/theask/theaskapi/service/impl/QuestionServiceImpl.java
@@ -2,13 +2,10 @@ package com.theask.theaskapi.service.impl;
 
 import com.theask.theaskapi.model.Question;
 import com.theask.theaskapi.repository.QuestionRepository;
-import com.theask.theaskapi.repository.QuestionRepositoryImpl;
 import com.theask.theaskapi.service.QuestionService;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,19 +17,18 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public List<Question> findAllSortByRating() {
-       return questionRepository.findAll()
-               .stream()
-               .sorted(Comparator.comparing(Question::getRating))
-               .collect(Collectors.toList())
-               ;
+        return questionRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Question::getRating))
+                .collect(Collectors.toList());
 
     }
+
     @Override
     public List<Question> findAllByTag(String tag) {
         return questionRepository.findAll()
                 .stream()
-                .filter(it->it.getTag().getText().equals(tag))
-                .collect(Collectors.toList())
-                ;
+                .filter(it -> it.getTag().getText().equals(tag))
+                .collect(Collectors.toList());
     }
 }
