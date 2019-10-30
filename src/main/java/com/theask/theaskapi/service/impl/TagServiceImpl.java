1dfa651aab9c76c3db99eebb1560abc1a2c4a879
Morlock <hungrymorlock1917@gmail.com>
2019-10-30 17:54:26 +0300

    TagController created

---
+++ src/main/java/com/theask/theaskapi/service/impl/TagServiceImpl.java
@@ -0,0 +1,26 @@
+package com.theask.theaskapi.service.impl;
+
+import com.theask.theaskapi.model.Tag;
+import com.theask.theaskapi.repository.TagRepository;
+import com.theask.theaskapi.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TagServiceImpl implements TagService {
+
+    private final TagRepository tagRepository;
+
+    @Override
+    public List<Tag> findAllSortByAlphabet() {
+        return tagRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Tag::getText))
+                .collect(Collectors.toList());
+    }
+}
