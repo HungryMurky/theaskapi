7b1074141830897c3cd6bc29d0f6b5efad192bcb
Morlock <hungrymorlock1917@gmail.com>
2019-12-21 13:13:46 +0300

    testing git in terminal

--- src/main/resources/db/migration/V1__create_tag_table.sql
+++ src/main/resources/db/migration/V1__create_tag_table.sql
@@ -4,4 +4,15 @@ create table tag
         primary key auto_increment,
     text varchar(255) null
 );
+create table question
+(
+    id     bigint(64)   not null auto_increment
+        primary key,
+    text   varchar(255) null,
+    rating bigint       null,
+    tag_id bigint(64)   null
+);
+alter table question
+    add foreign key question_tag_fkey (tag_id) references tag (id);
+
 
